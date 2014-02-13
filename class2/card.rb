ae149aea0714a962d136ffa38e7921f55a202fad
Curtis9 <curtis@lindenlab.com>
2014-02-12 17:00:59 -0800

    empty

---
+++ class2/card.rb
@@ -0,0 +1,14 @@
+class Card
+  def initialize(suite, value)
+
+  end
+
+  def suite
+  end
+
+  def value
+  end
+
+  def to_s
+  end
+end
---
+++ class2/hand.rb
@@ -0,0 +1,13 @@
+class Hand
+  def initialize
+  end
+
+  def add_card(card)
+  end
+
+  def to_s
+  end
+
+  def at(index)
+  end
+end
